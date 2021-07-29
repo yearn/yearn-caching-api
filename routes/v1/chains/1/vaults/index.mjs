cea6ff019ade764dbe625c9bda492bc8b85e8c0f
nymmrx <nymmrx@protonmail.com>
2021-07-29 19:36:03 +0200

    feat: basic functionality

--- routes/v1/chains/1/vaults/index.mjs
+++ routes/v1/chains/1/vaults/index.mjs
@@ -2,7 +2,8 @@
  * @param {import("fastify").FastifyInstance} api
  */
 export default async function (api) {
-  api.get("/", async () => {
-    return { message: "Welcome to the yearn.finance API." };
+  api.get("/all", async () => {
+    const vaults = api.sdk.vaults.get();
+    return vaults;
   });
 }
