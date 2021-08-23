1fca26843771fb9e495397849e72f5a9305bd163
jstash <19509999+jstashh@users.noreply.github.com>
2021-08-23 17:31:06 +0100

    fix: made jobs start immediately on startup

--- jobs/index.mjs
+++ jobs/index.mjs
@@ -5,9 +5,11 @@ export default [
   {
     name: "hello",
     interval: "1m",
+    timeout: 0,
   },
   {
     name: "vaults",
     interval: "7m",
+    timeout: 0,
   },
 ];
--- jobs/vaults.mjs
+++ jobs/vaults.mjs
@@ -3,11 +3,7 @@ import { cache } from "../plugins/caching.mjs";
 import { VaultsAllCacheKey, VaultsAllCacheTime } from "../routes/v1/chains/1/vaults/index.mjs";
 
 (async () => {
-  try {
-    const vaults = await yearn.vaults.get();
-    cache.set(VaultsAllCacheKey, vaults, VaultsAllCacheTime);
-  } catch (error) {
-    process.exit(1);
-  }
+  const vaults = await yearn.vaults.get();
+  cache.set(VaultsAllCacheKey, vaults, VaultsAllCacheTime);
   process.exit(0);
 })();
