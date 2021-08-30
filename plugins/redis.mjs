afed58a40c81b8375b9deb61328cbb6060d48ebd
jstash <19509999+jstashh@users.noreply.github.com>
2021-08-30 17:59:35 +0100

    fix: added fastify cors

--- plugins/redis.mjs
+++ plugins/redis.mjs
@@ -3,6 +3,7 @@ import fp from "fastify-plugin";
 import IORedis from "ioredis";
 
 export const redis = new IORedis({ host: "redis" });
+// export const redis = new IORedis({ host: "localhost", port: 6379 });
 
 /**
  * This plugins adds a shared redis connection
@@ -12,5 +13,10 @@ export const redis = new IORedis({ host: "redis" });
 export default fp(async function (api) {
   api.register(await import("fastify-redis"), {
     client: redis,
+  });
+
+  api.register(await import("fastify-cors"), {
+    origin: "*",
+    methods: "GET",
   });
 });
