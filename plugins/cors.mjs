19ce3b66024b91b63e93bca5af8e29a8540295fd
jstashh <19509999+jstashh@users.noreply.github.com>
2021-09-02 10:50:08 +0100

    fix: added cors plugin (#5)
    * fix: added fastify cors
    
    * fix: added cors plugin to its own file

---
+++ plugins/cors.mjs
@@ -0,0 +1,13 @@
+import fp from "fastify-plugin";
+
+/**
+ * This plugins allows manipulation of cors headers
+ *
+ * @see https://github.com/fastify/fastify-cors
+ */
+export default fp(async function (api) {
+  api.register(await import("fastify-cors"), {
+    origin: "*",
+    methods: "GET",
+  });
+});
