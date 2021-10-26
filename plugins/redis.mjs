be972b4c108d2f5fc122b86f1941950bb8c4a02b
jstash <19509999+jstashh@users.noreply.github.com>
2021-10-26 16:03:26 +0100

    fix: undo redis debugging setting

--- plugins/redis.mjs
+++ plugins/redis.mjs
@@ -2,7 +2,7 @@ import fp from "fastify-plugin";
 
 import IORedis from "ioredis";
 
-export const redis = new IORedis({ host: "localhost", port: 6379 });
+export const redis = new IORedis({ host: "redis" });
 
 /**
  * This plugins adds a shared redis connection
