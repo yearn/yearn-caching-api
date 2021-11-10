74216e5fdc0bc3b9ff81729acebf1f8ef5b2ac6d
jstash <19509999+jstashh@users.noreply.github.com>
2021-11-10 14:22:11 +0000

    fix: remove debugging code

--- jobs/index.mjs
+++ jobs/index.mjs
@@ -2,29 +2,29 @@
  * @see https://jobscheduler.net/#/
  */
 export default [
-  // {
-  //   name: "strategies",
-  //   interval: "7m",
-  //   timeout: 0,
-  // },
-  // {
-  //   name: "earnings",
-  //   interval: "7m",
-  //   timeout: 0,
-  // },
-  // {
-  //   name: "tokens",
-  //   interval: "7m",
-  //   timeout: 0,
-  // },
-  // {
-  //   name: "vaults",
-  //   interval: "6m",
-  //   timeout: 0,
-  // },
+  {
+    name: "strategies",
+    interval: "7m",
+    timeout: 0,
+  },
+  {
+    name: "earnings",
+    interval: "7m",
+    timeout: 0,
+  },
+  {
+    name: "tokens",
+    interval: "7m",
+    timeout: 0,
+  },
+  {
+    name: "vaults",
+    interval: "6m",
+    timeout: 0,
+  },
   {
     name: "ironbank",
-    interval: "10s",
-    timeout: 3000,
+    interval: "8m",
+    timeout: 0,
   },
 ];
